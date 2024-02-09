0899ce7ec25bb738db028c69d13f4660aa6fb4d4
Jose Perez <jotangamers@gmail.com>
2024-02-08 20:39:13 -0400

    Avanzando los operadores

--- Contenido/Capitulo-1/operadores.js
+++ Contenido/Capitulo-1/operadores.js
@@ -1,3 +1,31 @@
 // Las operaciones
 
 // Operadores de asignación y operadores aritmeticos.
+
+/* Hay diferentes tipos de operadores de asignacion en los que podemos encontrar estan: */
+
+x = y; // Asignación
+
+x += y; //Asignación de adicción 
+
+x -= y; //Asignación de sustracción
+
+x *= y; //Asignación de multiplicación
+
+x /= y; //Asignación de división 
+
+x %= y; // Asignación de resto
+
+x **= y; //Asignación de potenciación 
+
+x <<= y; //Asignación de desplazamiento a la izquierda
+
+x >>= y; //Asignación de desplazamiento a la derecha
+
+x >>>= y; //Asignación sin signo de desplazamiento a la derecha 
+
+x &= y; //Asignación AND
+
+x ^= y; //Aignación XOR
+
+x |= y; //Asignación OR 
